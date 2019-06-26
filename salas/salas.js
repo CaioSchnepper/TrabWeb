cf76ba74575ce3a0a904ade20d9f8659b7cf05da
Eric Ueta <eric-ueta@hotmail.com>
2019-06-26 04:08:52 -0300

    CRUD salas feito

--- salas/salas.js
+++ salas/salas.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-    var serverURL = 'http://localhost:3000/generos';
+    var serverURL = 'http://localhost:3000/salas';
     var $tcorpo = $("#tcorpo");
     var $modalAdicionar = $("#modalAdicionar");
     var $modalAlterar = $("#modalAlterar");
     var $formAlterar = $("#formAlterar");
-    function addGenero(genero){
-        $tcorpo.append("<tr><th scope='row'>" + genero.id +"</th><td class='genre'>"+ genero.genero+"</td><td>"+
-        "<button type='button' class='editarGenero btn btn-warning' data-id='"+genero.id+
+    function addSala(sala){
+        $tcorpo.append("<tr><th scope='row'>" + sala.id +"</th><td class='lot_max'>"+ sala.lotacao_maxima+"</td><td>"+
+        "<button type='button' class='editarLot btn btn-warning' data-id='"+sala.id+
         "'>Alterar</button></td><td><button type='button' class='apagar btn btn-danger' data-id='"
-        +genero.id+"'>Apagar</button></td></tr>");
+        +sala.id+"'>Apagar</button></td></tr>");
     }
     
     // CARREGAR 
@@ -16,8 +16,8 @@ $(document).ready(function(){
         type: 'GET',
         url: serverURL,
         success: function(data){
-            $.each(data, function(i,genero){
-                addGenero(genero);
+            $.each(data, function(i,sala){
+                addSala(sala);
             })
         },
     });
@@ -25,12 +25,12 @@ $(document).ready(function(){
     // APAGAR
     $tcorpo.delegate(".apagar","click",function(){
         var $tr = $(this).closest("tr");
-        if(confirm("Você realmente deseja deletar esse gênero?")){
+        if(confirm("Você realmente deseja deletar essa sala?")){
             $.ajax({
                 type: 'DELETE',
                 url: serverURL + "/" + $(this).attr("data-id"),
                 success: function(){
-                    alert("Gênero removido com sucesso");
+                    alert("Sala removida com sucesso");
                     $tr.fadeOut(300, function(){
                         $(this).remove();
                     });
@@ -40,60 +40,60 @@ $(document).ready(function(){
     });
 
     // EDITAR
-    $tcorpo.delegate(".editarGenero","click",function(){
+    $tcorpo.delegate(".editarLot","click",function(){
         var linha = this.parentNode.parentNode;
         $modalAlterar.modal('show');
         
         $formAlterar.on('submit', function(form){
             form.preventDefault();
             form.stopImmediatePropagation();
-            var editGenero = document.querySelector("#inputEditGenero");
+            var editLot = document.querySelector("#inputEditLotacao");
         
-            if (editGenero.value == '' || editGenero.value == null) {
-                editGenero.classList.add('border-danger');
+            if (editLot.value == '' || editLot.value == null) {
+                editLot.classList.add('border-danger');
                 return;
             } else {
-                var genre = { 
-                    genero: editGenero.value,
+                var lot = {
+                    lotacao_maxima: editLot.value,
                 };
                 $.ajax({
                     url: serverURL + "/" + linha.firstChild.textContent,
                     type: 'PUT',    
-                    data: genre,
+                    data: lot,
                     success: function(response) {
-                        alert("Gênero alterado com sucesso");
-                        linha.querySelector('.genre').textContent = editGenero.value;
-                        editGenero.classList.remove('border-danger');
-                        editGenero.value = '';
+                        alert("Lotação alterada com sucesso");
+                        linha.querySelector('.lot_max').textContent = editLot.value;
+                        editLot.classList.remove('border-danger');
+                        editLot.value = '';
                         $modalAlterar.modal('hide');
                     }
                 });
             }
         }); 
     });
- 
+   
     // ADICIONAR
     $modalAdicionar.delegate(".adicionar","click",function(){
-        var addGenre = document.querySelector("#inputAddGenero");
-        if (addGenre.value == '' || addGenre.value == null) {
-            addGenre.classList.add('border-danger');
+        var addLotacao = document.querySelector("#inputAddLotacao");
+        if (addLotacao.value == '' || addLotacao.value == null) {
+            addLotacao.classList.add('border-danger');
             return;
         } else {
-            addGenre.classList.remove('border-danger');
-            addGenre.classList.add('border-success');
-            var genre = {
-                genero: addGenre.value,
+            addLotacao.classList.remove('border-danger');
+            addLotacao.classList.add('border-success');
+            var nSala = {
+                lotacao_maxima: addLotacao.value,
             };
-            addGenre.classList.remove('border-success');
+            addLotacao.classList.remove('border-success');
             $.ajax({
                 type: 'POST',
                 url: serverURL,
-                data: genre,
+                data: nSala,
                 success: function(adicionar){
                     $("#modalAdicionar").modal('hide');
-                    addGenero(adicionar);
-                    alert("Gênero adicionado com sucesso");  
-                    addGenre.value = '';
+                    addSala(adicionar);
+                    alert("Sala adicionada com sucesso");  
+                    addLotacao.value = '';
                 }
             });
         }
